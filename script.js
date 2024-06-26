524d50b06ec7c19f7e5ada094ff455d61411304e
KyannaG <100180586+KyannaG@users.noreply.github.com>
2024-06-26 17:53:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,22 @@ const suggestions = document.querySelector('.suggestions ul');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
-function search(str) {
+function search(input) {
 	return fruit.filter(option =>
-		fruit.toLowerCase().includes(input.toLowerCase()) 
+		option.toLowerCase().includes(input.toLowerCase()) 
 		);
 }
 
 function searchHandler(e) {
 	const unserInput = e.target.value.trim();
+	
+	if (userInput === '') {
+		alert('Input empty');
+		suggestionsList.innerHTML = ''; // Clear suggestions list
+		suggestionsList.parentNode.classList.remove('has-suggestions'); // Hide suggestions list
+		return;
+	  }
+
 	const results = search(userInput);
 	showSuggestions(results, userInput);
  
