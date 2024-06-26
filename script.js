33c5a670e68c5dbc0c6076b30d867528ebdca90c
KyannaG <100180586+KyannaG@users.noreply.github.com>
2024-06-26 18:07:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const input = document.querySelector('#fruit');
-const suggestions = document.querySelector('.suggestions ul');
+const suggestionsList = document.querySelector('#fruit-list');
 
 const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 
@@ -10,8 +10,8 @@ function search(input) {
 }
 
 function searchHandler(e) {
-	const unserInput = e.target.value.trim();
-	
+	const userInput = e.target.value.trim();
+
 	if (userInput === '') {
 		alert('Input empty');
 		suggestionsList.innerHTML = ''; // Clear suggestions list
@@ -19,14 +19,40 @@ function searchHandler(e) {
 		return;
 	  }
 
-	const results = search(userInput);
-	showSuggestions(results, userInput);
- 
 
+	const results = search(userInput);
+	showSuggestions(results);
+
+ 
 	// TODO
 }
 
 function showSuggestions(results) {
+	suggestionsList.innerHTML = ''; 
+	if (results.length > 0) {
+		results.forEach(result => {
+		  const li = document.createElement('li');
+		  li.textContent = result;
+		  suggestionsList.appendChild(li);
+		});
+		suggestionsList.parentNode.classList.add('has-suggestions');
+	  } else {
+		suggestionsList.parentNode.classList.remove('has-suggestions');
+	  }
+	}
+	// TODO
+
+function useSuggestion(e) {
+	if (e.target.tagName === 'LI') {
+		input.value = e.target.textContent;
+		suggestionsList.innerHTML = '';
+		suggestionsList.parentNode.classList.remove('has-suggestions');
+	}
+	// TODO
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestionsList.addEventListener('click', useSuggestion);s) {
 	if (results.length > 0) {
 		results.forEach(result => {
 		  const li = document.createElement('li');
